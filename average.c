1ed3e7bc434b235e0f594f5a3295b4ba5a95a6e9
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-16 01:10:06 +0530

    average......

---
+++ average.c
@@ -0,0 +1,13 @@
+// write a program to calculate average of 3 numbers
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a,b,c,average;
+       printf("three number is  ");
+       scanf("%d %d %d",&a,&b,&c);
+    
+    average=(a+b+c)/3;
+    printf("average is %d:",average);
+    return 0;
+}
