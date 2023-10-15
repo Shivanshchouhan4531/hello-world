1056e28b674945253ce009439c1a6e21dad2bcd1
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-16 00:50:13 +0530

    area of square

---
+++ square.c
@@ -0,0 +1,12 @@
+// write a program to calculate area of squre
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int side,area;
+    printf("enter side:");
+    scanf("%d",&side);
+    area = side *side;
+    printf("area of square : %d",area);
+    return 0;
+}
