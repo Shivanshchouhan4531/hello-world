639445716a8d5c2840e7e8f19f37d62fe0c3d1f5
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-15 23:24:57 +0530

    Create intrest.c

---
+++ intrest.c
@@ -0,0 +1,15 @@
+//write to simple intrest 
+#include <stdio.h>
+
+int main() {
+   int p;
+   float t,r,si;
+   p=80000;
+   t=1;
+   r=10;
+   si=(p*r*t)/100;
+   printf("interst is :%f\n",si);
+   return 0;
+}
+
+output 8000
