b633e946ca38e6088c67e11521efe959bae5b812
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-17 00:50:19 +0530

    reverse number using loops

---
+++ reverse.c
@@ -0,0 +1,27 @@
+//reverse the number 
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("enter number:");
+    scanf("%d",&n);
+    
+    for(i=n;i>=1;i--){
+        printf("%d\n",i);
+        
+    }
+    return 0;
+    
+}
+output:---
+  
+enter number:10
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
