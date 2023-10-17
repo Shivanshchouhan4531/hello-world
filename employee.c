4f10f10046bc159dbceae7d5aa16d1b0d1383cac
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-17 22:21:44 +0530

    Create employee.c

---
+++ employee.c
@@ -0,0 +1,24 @@
+//find the gross salary of imployee
+#include<stdio.h>
+int main(){
+    int bs,hra,da,gs;
+    
+    printf("enter the basic salary:");
+    scanf("%d",&bs);
+    
+    if(bs<1500){
+        hra =bs*10/100;
+        da = bs*90/100;
+    }
+    else{
+        hra=500;
+        da= bs*98/100;
+    }
+        
+    gs=hra+da+bs;
+        printf("gross salary is : %d",gs);
+        
+        return 0;
+} 
+output :- enter the basic salary:2000
+          gross salary is : 4460
