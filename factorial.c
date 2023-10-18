24280a4b7476d983b19fb2b92f717ebca8f334c9
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-18 23:25:09 +0530

    factorial using while loop

---
+++ factorial.c
@@ -0,0 +1,16 @@
+// write a factorial program using loops
+#include<stdio.h>
+int main(){
+    int i=1,fact =1;
+    int num;
+    
+    printf("enter the number you want for factorial:");
+    scanf("%d",&num);
+    while(num>=i){
+        fact = fact*i;
+        i++;
+    }
+    printf("factorial is :%d\n",fact);
+    return 0;
+    
+}
