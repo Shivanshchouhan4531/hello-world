2ecaeb037552a4100edcb314ebac056c574659fa
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-19 01:54:58 +0530

    star pattern using loops

---
+++ star.c
@@ -0,0 +1,15 @@
+//print star pattern using loops
+#include<stdio.h>
+int main(){ 
+    
+          int i,j;
+          
+          for(i=1;i<=4;i++){
+              for(j=1;j<=8;j++){
+                  printf("*");
+              }
+              printf("\n");
+          }
+          return 0;
+    
+}
