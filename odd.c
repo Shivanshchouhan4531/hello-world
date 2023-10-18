c229d4ad00edafa6a1c96fed24d9b7ad3a42ecc0
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-19 01:11:23 +0530

    print 5 to 50 odd number udig contiue

---
+++ odd.c
@@ -0,0 +1,15 @@
+// write a program to print 5 to 50 odd nmbers using continue statment
+#include<stdio.h>
+int main (){
+    
+    int i;
+    for(i=5;i<=50;i++){
+        if(i%2==0){
+            continue ;
+            
+        }
+        printf("%d\n",i);
+    }
+     
+    return 0;
+}
