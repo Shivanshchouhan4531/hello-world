4f52dbbf579eebe382f10705003dd3bc86cd8c3c
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-19 01:43:30 +0530

    Create sum of n .c
    sum of num between  5 to 50

---
+++ sum of n .c
@@ -0,0 +1,13 @@
+//write a program to sum of all number 5 to 50
+#include<stdio.h>
+int main(){
+    int i,n,sum =0;
+   
+    for(i=5;i<=50;i++){
+        sum= sum +i;
+        
+    }
+    printf("sum is :%d\n",sum);
+    
+    return 0;
+}
