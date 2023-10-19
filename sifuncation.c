661974838d0e184ffa256ead6e0c071f5f7203c7
shivansh chouhan <147972841+Shivanshchouhan4531@users.noreply.github.com>
2023-10-19 21:14:41 +0530

    printf the statement using funcation

---
+++ sifuncation.c
@@ -0,0 +1,13 @@
+// simple program using funcations
+#include <stdio.h>
+
+void printhello(); //declare the funcation
+int main(){
+    
+  printhello(); // call the funcation
+
+    return 0;
+}
+void printhello(){ // define the funcation
+          printf("hello myself shivansh\n");
+}
